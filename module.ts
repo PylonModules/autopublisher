52c9f917ab5896e8df1524d67569d76482492fc2
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-03-02 09:48:36 -0600

    Added a toggle to publish or not

--- module.ts
+++ module.ts
@@ -1,24 +1,15 @@
 import * as config from '../config';
 
 discord.on(discord.Event.MESSAGE_CREATE, async (message) => {
-  if (config.autopublisher.channels.includes(message.channelId)) {
-    var channel = await discord.getGuildNewsChannel(message.channelId);
-    if (!channel)
-      throw new Error(
-        '[AUTOPUBLISHER] Channel with id ' +
-          message.channelId +
-          ' is not a news channel.'
-      );
-    channel.publishMessage(message.id);
-    message
-      .inlineReply(
-        'Successfully published message!\n\n*This message will delete in 3 seconds*'
-      )
-      .then((msg) => {
-        setTimeout(() => {
-          msg.delete();
-        }, 3000);
-      });
-      
-  }
+  if (!config.autopublisher.channels.includes(message.channelId)) return;
+  if (message.content.includes('--nopublish')) return;
+  
+  var channel = await discord.getGuildNewsChannel(message.channelId);
+  if (!channel)
+    throw new Error(
+      '[AUTOPUBLISHER] Channel with id ' +
+        message.channelId +
+        ' is not a news channel.'
+    );
+  channel.publishMessage(message.id);
 });
